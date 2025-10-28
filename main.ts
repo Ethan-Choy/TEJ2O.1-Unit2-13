9b26908e1b42dd3147480365a190923ac4edd858
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-10-28 13:30:13 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables 
+let neopixelStrip: neopixel.Strip = null
+let counter: number = 0
+
+//cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function() {
+    // turn on neopixels 
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+while (counter < 4) {
+     neopixelStrip.setPixelColor(counter, neopixel.colors(NeoPixelColors.Black)) }
+    neopixelStrip.show()
+    counter = counter +1
+}
+})
